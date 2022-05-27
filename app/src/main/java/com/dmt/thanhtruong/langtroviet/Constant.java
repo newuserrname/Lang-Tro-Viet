07f338caf8ddb65658df74d4ad1caabd7505867a
tuiilatruong <76780807+tuiilatruong@users.noreply.github.com>
2022-05-27 11:09:41 +0700

    GiaBao Update layout

--- app/src/main/java/com/dmt/thanhtruong/langtroviet/Constant.java
+++ app/src/main/java/com/dmt/thanhtruong/langtroviet/Constant.java
@@ -1,11 +1,12 @@
 package com.dmt.thanhtruong.langtroviet;
 
 public class Constant {
-    public static final String URL = "http://192.168.1.43:8080/";
+    public static final String URL = "http://langtro.ml/";
     public static final String HOME = URL+"api";
     public static final String LOGIN = HOME+"/login";
     public static final String REGISTER = HOME+"/register";
     public static final String SAVE_USER_INFO = HOME+"/save_user_info";
     public static final String POSTS = HOME+"/posts";
     public static final String ADD_POST = POSTS+"/create";
+    public static final String UPDATE_POST = POSTS+"/update";
 }
