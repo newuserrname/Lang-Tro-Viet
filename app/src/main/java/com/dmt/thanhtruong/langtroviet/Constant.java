767522900078abc278d2d85d05b0bec58b91f776
tuiilatruong <76780807+tuiilatruong@users.noreply.github.com>
2022-06-04 12:48:38 +0700

    Update Profile 50%

--- app/src/main/java/com/dmt/thanhtruong/langtroviet/Constant.java
+++ app/src/main/java/com/dmt/thanhtruong/langtroviet/Constant.java
@@ -5,8 +5,10 @@ public class Constant {
     public static final String HOME = URL+"api";
     public static final String LOGIN = HOME+"/login";
     public static final String REGISTER = HOME+"/register";
+    public static final String LOGOUT = HOME+"/logout";
     public static final String SAVE_USER_INFO = HOME+"/save_user_info";
     public static final String POSTS = HOME+"/posts";
+    public static final String MY_POSTS = POSTS+"/my_posts";
     public static final String ADD_POST = POSTS+"/create";
     public static final String UPDATE_POST = POSTS+"/update";
     public static final String DELETE_POST = POSTS+"/delete";
